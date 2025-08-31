9d0c12ee6ffa47f6e83fea9d7d1d0c635dcd640d
developereleven11-create <developer.eleven11@gmail.com>
2025-08-31 21:35:43 +0530

    Update StatusBadge.tsx

--- components/StatusBadge.tsx
+++ components/StatusBadge.tsx
@@ -1,1 +1,13 @@
-export default function StatusBadge({status}:{status:string}){return <span className='px-2 py-1 bg-gray-700 rounded'>{status}</span>}
+export default function StatusBadge({ status }: { status: string }) {
+  let color = 'bg-gray-600';
+  if (/RTO/i.test(status)) color = 'bg-red-500';
+  else if (/Transit/i.test(status)) color = 'bg-orange-500';
+  else if (/OFD|Delivery/i.test(status)) color = 'bg-green-500';
+  else if (/Not Settled/i.test(status)) color = 'bg-yellow-500';
+
+  return (
+    <span className={`${color} text-xs px-2 py-1 rounded-full text-white`}>
+      {status}
+    </span>
+  );
+}
