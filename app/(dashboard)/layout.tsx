04ba1e8e6b5e93f4858c2ca1338fb4b503aa414a
developereleven11-create <developer.eleven11@gmail.com>
2025-09-01 14:42:31 +0530

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,21 +1,13 @@
-import Sidebar from "../components/Sidebar";
-import Topbar from "../components/Topbar";
+import Sidebar from "../../components/Sidebar";
+import Topbar from "../../components/Topbar";
 
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
-      {/* Sidebar */}
       <Sidebar />
-
-      {/* Main content area */}
-      <div className="flex-1 bg-gray-950 min-h-screen text-white">
+      <div className="flex-1 ml-64">
         <Topbar />
-        <main className="p-6">{children}</main>
+        <main className="pt-16 p-6">{children}</main>
       </div>
     </div>
   );
