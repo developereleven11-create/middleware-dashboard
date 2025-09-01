a758ab19537fdaedd3848aec3400f3d8a831fd6a
developereleven11-create <developer.eleven11@gmail.com>
2025-09-01 13:03:52 +0530

    Create layout.tsx

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,21 @@
+import Sidebar from "../components/Sidebar";
+import Topbar from "../components/Topbar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main Content with Topbar */}
+      <div className="flex-1 bg-gray-950 min-h-screen text-white">
+        <Topbar />
+        <main className="p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
