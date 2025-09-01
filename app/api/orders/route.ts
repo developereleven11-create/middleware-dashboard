9479b7ab56c6ca8948ca0b584d7caef06251a409
developereleven11-create <developer.eleven11@gmail.com>
2025-09-01 16:12:16 +0530

    Update route.ts

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 // Fetch orders from Shopify → Save into DB → Return response
 export async function GET() {
