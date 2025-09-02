91e8044632468e07ac2895b5f1682e2fe78dad95
developereleven11-create <developer.eleven11@gmail.com>
2025-09-02 10:56:35 +0530

    Update route.ts

--- app/api/jobs/syncShopify/route.ts
+++ app/api/jobs/syncShopify/route.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/prisma";
 
 export async function GET() {
   try {
-    const res = await fetch("https://your-shopify-url/admin/api/2023-10/orders.json?limit=50", {
+    const res = await fetch("https://pokonut.myshopify.com/admin/api/2023-10/orders.json?limit=50", {
       headers: {
         "X-Shopify-Access-Token": process.env.SHOPIFY_ACCESS_TOKEN!,
       },
